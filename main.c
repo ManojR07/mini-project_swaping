b828160e806000a0994767ac917f1982dfdc5b21
ManojR07 <104481472+ManojR07@users.noreply.github.com>
2022-04-27 10:15:26 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+int main() {
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  // value of first is assigned to temp
+  temp = first;
+
+  // value of second is assigned to first
+  first = second;
+
+  // value of temp (initial value of first) is assigned to second
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+  return 0;
+}
